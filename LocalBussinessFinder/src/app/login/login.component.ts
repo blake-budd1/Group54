49a06aa4d4b06bdf48472d5da271996df9f4e9f1
Wavid Bowman <102263445+wavesdog470@users.noreply.github.com>
2023-02-08 18:10:04 -0500

    Updated files and made the beginning of a login page

---
+++ LocalBussinessFinder/src/app/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { LoginList } from '../lfile';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  user = new LoginList('Enter Username', 'Enter Password');
+  submitted = false;
+  onSubmit() {
+    this.submitted = true;
+  }
+}
