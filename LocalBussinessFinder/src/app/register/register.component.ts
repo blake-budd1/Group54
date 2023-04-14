e0181561295f6535f05cda2b2af99ea2f8877067
Wavid Bowman <wav.dimportant@gmail.com>
2023-04-14 12:40:56 -0400

    Changed this stuff

--- LocalBussinessFinder/src/app/register/register.component.ts
+++ LocalBussinessFinder/src/app/register/register.component.ts
@@ -1,10 +1,11 @@
-import { Component, Inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { RegisterList } from '../lfile';
 import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, ignoreElements } from 'rxjs/operators';
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA, } from '@angular/material/dialog';
-
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { error_popup } from '../Error_Popup/error_popup.component';
 
 @Component({
   selector: 'app-register',
@@ -12,9 +13,10 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA, } from '@angular/material/dia
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
-  constructor(private http: HttpClient, public dialog: MatDialog){}
+  constructor(private http: HttpClient, public dialog: MatDialog, private router: Router){}
   user = new RegisterList('Enter Email', 'Enter Username', 'Enter Password', 'Confirm Password');
   submitted = false;
+  errorType = "";
   onSubmit() {
     return this.http.post('api/register', this.user).pipe(
       catchError(error => {
@@ -26,21 +28,31 @@ export class RegisterComponent {
       if(regObj.Reg_State == "Successful"){
         // CODE FOR SUCCESSFUL REGISTRATION
         console.log("Registration Successful")
+        this.router.navigate(['Setup'])
       }
       else if(regObj.Reg_State == "Email_Registered"){
         //CODE FOR EMAIL ALREADY REGSITERED
         console.log("Email Already Registered")
+          //CODE FOR INCORRECT PASSWORD 
+          console.warn("Email In use -> implement warning)")
+          this.errorType = "Email"
+          this.displayError();
+        
       }
       else if(regObj.Reg_State == "Unmatched Password"){
         //NOTIFY USER PASSWORDS DON'T MATCH
         console.log("Passwords don't match")
+        console.warn("Password Mismatch -> implement warning)")
+        this.errorType = "NotMatch"
+        this.displayError();
       }
       else if(regObj.Reg_State == "Username_Taken"){
         //NOTIFY USER THE USERNAME IS TAKEN
         console.log("Username Taken")
+        console.warn("User in use-> implement warning)")
+        this.errorType = "UserUsed"
+        this.displayError(); 
       }
-
-
       console.log(response);
     });}
 
@@ -55,5 +67,12 @@ export class RegisterComponent {
   }
   setConfirmPassword(val4: string){
     this.user.confirmPassword = val4;
+  }
+  displayError(){
+    const dialogRef = this.dialog.open(error_popup, {
+      width: "600px",
+      height: "180px",
+      data: {useCase: this.errorType}
+    });
   }
 }
